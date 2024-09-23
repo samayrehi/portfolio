04aad9a126ce5c0f1dd681f4ea109d3b7850ee10
Samay Rehi <samaymukeshrehi@gmail.com>
2024-09-23 19:40:38 +0530

    am

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import project2 from "./assets/currency.jpg";
 import project3 from "./assets/port.jpg";
 import project4 from "./assets/dash.jpg";
 
-export const HERO_CONTENT = `I am a passionate Full Stack Developer skilled in MERN Stack technologies like ReactJS, NodeJS, ExpressJS, and MongoDB. I love creating responsive, dynamic web applications with a focus on both front-end and back-end development. With expertise in HTML, CSS, Tailwind-CSS, and experience using Git-GitHub for version control, I am eager to dive into real-world projects. As a fresher, I amm ready to leverage my knowledge and skills to solve complex problems and build innovative digital solutions.`;
+export const HERO_CONTENT = `I am a passionate Full Stack Developer skilled in MERN Stack technologies like ReactJS, NodeJS, ExpressJS, and MongoDB. I love creating responsive, dynamic web applications with a focus on both front-end and back-end development. With expertise in HTML, CSS, Tailwind-CSS, and experience using Git-GitHub for version control, I am eager to dive into real-world projects. As a fresher, I am ready to leverage my knowledge and skills to solve complex problems and build innovative digital solutions.`;
 
 export const PART4 = `As a fresher, I am excited to apply my skills in real-world projects and continue learning. My certification not only validates my technical knowledge but also reflects my commitment to growth and development in web technologies.`;
 
-export const PART1 = `Hello! I’m a certified Full Stack Developer with a focus on MERN Stack technologies. I hold a Completion Certificate in MERN Stack Development from MasyncTech Coding School on Udemy, where I gained hands-on experience with JavaScript, ReactJS, NodeJS, ExpressJS, and MongoDB. This certification has equipped me with the skills to build full-stack web applications that are both dynamic and responsive.`;
+export const PART1 = `Hello! I am a certified Full Stack Developer with a focus on MERN Stack technologies. I hold a Completion Certificate in MERN Stack Development from MasyncTech Coding School on Udemy, where I gained hands-on experience with JavaScript, ReactJS, NodeJS, ExpressJS, and MongoDB. This certification has equipped me with the skills to build full-stack web applications that are both dynamic and responsive.`;
 
 export const PROJECTS = [
   {
